a4f9a7f6d89498a0455a272d10ad472189ef755f
tanirikay <tanirikay@gmail.com>
2026-03-28 17:49:05 +0800

    Add files via upload
    this one has the attributes for the game

---
+++ Combatant.java
@@ -0,0 +1,41 @@
+package sc2002project;
+
+import java.util.*;
+public abstract class Combatant {
+	protected String name;
+    protected int hp;
+    protected int maxHp;
+    protected int attack;
+    protected int defense;
+    protected int speed;
+    protected int specialSkillCooldown = 0; 
+    protected List<String> activeStatusEffects = new ArrayList<>(); 
+
+    public Combatant(String name, int hp, int attack, int defense, int speed) {
+        this.name = name;
+        this.hp = hp;
+        this.maxHp = hp;
+        this.attack = attack;
+        this.defense = defense;
+        this.speed = speed;
+    }
+
+    // Common behavior: Taking damage 
+    // Damage formula: $Damage = \max(0, AttackerAttack - TargetDefense)$ 
+    public void takeDamage(int damage) {
+        this.hp = Math.max(0, this.hp - damage); 
+    }
+
+    public boolean isAlive() {
+        return this.hp > 0;
+    }
+
+    // Getters for stats 
+    public int getSpeed() { return speed; }
+    public String getName() { return name; }
+    public int getHp() { return hp; }
+    
+    // Abstract method: Each type decides how to act
+    public abstract void performTurn(BattleEngine engine);
+
+}
